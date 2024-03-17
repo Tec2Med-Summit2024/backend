5f7558574c70ae87748d251b0c9f82e578ea8465
Quark03 <quark03.dev@gmail.com>
2024-03-17 21:02:15 +0000

    Initial Commit

---
+++ app.mjs
@@ -0,0 +1,7 @@
+import express from "express"
+
+const app = express()
+
+app.get("/", (req, res) => res.json("Hello World!"));
+
+app.listen(9999, () => console.log(`Server ready at http://localhost:9999`))
