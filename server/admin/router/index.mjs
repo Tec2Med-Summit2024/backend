c4c212ce80eb3d6f49729a13809ca34fc87ea128
António Festas <antonio.c.festas@gmail.com>
2024-06-21 10:40:10 +0100

    Created more admin panel pages

--- server/admin/router/index.mjs
+++ server/admin/router/index.mjs
@@ -8,4 +8,24 @@ router.get("/", (req, res) => {
     return res.render("home", { title: "Admin Panel" })
 })
 
+router.get("/tickets", (req, res) => {
+    return res.render("tickets", { title: "Tickets Management" })
+})
+
+router.get("/events", (req, res) => {
+    return res.render("events", { title: "Events Management " })
+})
+
+router.get("/users", (req, res) => {
+    return res.render("users", { title: "Users Management" })
+})
+
+router.get("/attendees", (req, res) => {
+    return res.render("attendees", { title: "Attendees Management" })
+})
+
+router.get("/partners", (req, res) => {
+    return res.render("partners", { title: "Partners Management" })
+})
+
 export default router;
