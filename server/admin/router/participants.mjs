5a75a6eb8958467bb79d7a9a0df6b5ef45fe4a4b
António Festas <antonio.c.festas@gmail.com>
2024-11-19 22:10:45 +0000

    Created more pages for partner & participants

--- server/admin/router/participants.mjs
+++ server/admin/router/participants.mjs
@@ -5,12 +5,51 @@ const router = Router();
 
 router.get("/", async (req, res) => {
     try {
-        const participant = await makeQuery("MATCH (u:Participant) RETURN u")
+        const participants = await makeQuery("MATCH (u:Participant) RETURN u")
 
-        return res.render("participants/index", { title: "Participants Management", participants: [...participant] })
+        console.log(participants[0])
+
+        return res.render("participants/index", { title: "Participants Management", participants: [...participants] })
     } catch (error) {
         return res.status(500).send("Internal Server Error")
     }
+})
+
+router.get("/create", async (req, res) => {
+    try {
+        return res.render("participants/create", { title: "Create New Participant" })
+    } catch (error) {
+        return res.status(500).send("Internal Server Error")
+    }
+})
+
+router.post("/create", async (req, res) => {
+    try {
+        return res.redirect("/admin/participants")
+    } catch (error) {
+        return res.status(500).send("Internal Server Error")
+    }
+})
+
+router.get("/:id", async (req, res) => {
+    try {
+        const { id } = req.params
+
+        const participant = await makeQuery("MATCH (u:Participant) WHERE ID(u) = $id RETURN u", { id: id })
+
+        return res.render("participants/details", { title: "Participant Details", participant })
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send("Internal Server Error")
+    }
+})
+
+router.get("/:id/edit", (req, res) => {
+    return res.redirect("/admin/participants/:id")
+})
+
+router.post("/:id/edit", (req, res) => {
+    return res.redirect("/admin/participants/:id")
 })
 
 export default router;
--- server/admin/router/partners.mjs
+++ server/admin/router/partners.mjs
@@ -7,10 +7,49 @@ router.get("/", async (req, res) => {
     try {
         const partners = await makeQuery("MATCH (p:Partner) RETURN p")
 
+        console.log(partners[0])
+
         return res.render("partners/index", { title: "Partners Management", partners: [...partners] })
     } catch (error) {
         return res.status(500).send("Internal Server Error")
     }
+})
+
+router.get("/create", async (req, res) => {
+    try {
+        return res.render("participants/create", { title: "Create New Participant" })
+    } catch (error) {
+        return res.status(500).send("Internal Server Error")
+    }
+})
+
+router.post("/create", async (req, res) => {
+    try {
+        return res.redirect("/admin/partners")
+    } catch (error) {
+        return res.status(500).send("Internal Server Error")
+    }
+})
+
+router.get("/:id", async (req, res) => {
+    try {
+        const { id } = req.params
+
+        const partner = await makeQuery("MATCH (p:Partner) WHERE ID(p) = $id RETURN p", { id: id })
+
+        return res.render("partners/details", { title: "Partner Details", partner })
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send("Internal Server Error")
+    }
+})
+
+router.get("/:id/edit", (req, res) => {
+    return res.redirect("/admin/partners/:id")
+})
+
+router.post("/:id/edit", (req, res) => {
+    return res.redirect("/admin/partners/:id")
 })
 
 export default router;
